29d6b4213b7e9097f7496815fb0d2ec659309096
Milan Hauth <milahu@milahu.duckdns.org>
2026-03-08 17:40:42 +0100

    add scripts

---
+++ scripts/git-remote-add.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+owner=milahu
+repo=contact
+
+function git_remote_add_onion() {
+  local remote="$1"
+  local url="$2"
+  git remote add "$remote" "$url" ||
+  git remote set-url "$remote" "$url"
+  git config --add remote."$remote".proxy socks5h://127.0.0.1:9050
+}
+
+git_remote_add_onion darktea.onion http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/$owner/$repo
+
+git_remote_add_onion righttoprivacy.onion http://gg6zxtreajiijztyy5g6bt5o6l3qu32nrg7eulyemlhxwwl6enk6ghad.onion/$owner/$repo
+
+git_remote_add_onion gdatura.onion http://gdatura24gtdy23lxd7ht3xzx6mi7mdlkabpvuefhrjn4t5jduviw5ad.onion/$owner/$repo
+
+git_remote_add_onion darkforest.onion http://git.dkforestseeaaq2dqz2uflmlsybvnq2irzn4ygyvu53oazyorednviid.onion/$owner/$repo
